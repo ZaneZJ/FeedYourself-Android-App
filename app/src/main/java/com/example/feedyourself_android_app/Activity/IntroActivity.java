fdba43c72b21274c1f92f8461487c9bb04dae426
Zane <76492358+ZaneZJ@users.noreply.github.com>
2021-12-03 15:43:53 +0100

    Updated Intro view

--- app/src/main/java/com/example/feedyourself_android_app/Activity/IntroActivity.java
+++ app/src/main/java/com/example/feedyourself_android_app/Activity/IntroActivity.java
@@ -1,16 +1,30 @@
 package com.example.feedyourself_android_app.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.feedyourself_android_app.MainActivity;
 import com.example.feedyourself_android_app.R;
 
 public class IntroActivity extends AppCompatActivity {
+
+    private ConstraintLayout startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        startBtn.findViewById(R.id.startbtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            }
+        });
     }
 }
